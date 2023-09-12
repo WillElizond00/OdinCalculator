d54a05843f975a36c48b34b00748739c2b43cdc7
Steam Deck User <deck@steamdeck.attlocal.net>
2023-09-12 17:16:06 -0400

    messed up but looks ok

--- CalculatorLogic.js
+++ CalculatorLogic.js
@@ -1,57 +1,73 @@
+let isNewCalculation = true;
+let previousInput = "";
 
+document.addEventListener("DOMContentLoaded", function(){
 
-function add(a, b){
-    return a + b;
-}
-
-function subtract(a, b){
-    return a - b;
-}
-
-function multiply(a, b){
-    return a * b;
-}
-
-function division(a, b){
-    return a / b;
-}
-
-const operate = function (operator, a, b) {
-    switch (operator) {
-        case '+':
-            return add(a, b);
-        case '-':
+    
+    function add(a, b){
+        return a + b;
+    }
+    
+    function subtract(a, b){
+        return a - b;
+    }
+    
+    function multiply(a, b){
+        return a * b;
+    }
+    
+    function division(a, b){
+        return a / b;
+    }
+    
+    const operate = function (operator, a, b) {
+        a = parseFloat(a);
+        b = parseFloat(b);
+        
+        switch (operator) {
+            case '+':
+                return add(a, b);
+                case '-':
             return subtract(a, b);
         case '*':
             return multiply(a, b);
-        case '/':
-            return division(a, b);
-        default:
-            return 'Invalid operator';
-    }
-}
-
-let firstNumber = null;
-let secondNumber = null;
-let operator = null;
-let isOperatorClicked = false;
-
-const addButton = document.getElementById("add");
-const subtractButton = document.getElementById("subtract");
-const timesButton = document.getElementById("multiply");
-const divButton = document.getElementById("divide");
-const equalsButton = document.getElementById("equals");
-const clearButton = document.getElementById("clear");
-const decimalButton = document.getElementById("decimal")
-
-function handleOperatorClick(op) {
+            case '/':
+                return division(a, b);
+                default:
+                    return 'Invalid operator';
+                }
+            }
+            
+            let firstNumber = null;
+            let secondNumber = null;
+            let operator = null;
+            let isOperatorClicked = false;
+            
+            console.log("First Number:", firstNumber);
+            console.log("Second Number:", secondNumber);
+            console.log("Operator:", operator);
+            
+            
+            const addButton = document.getElementById("add");
+            const subtractButton = document.getElementById("subtract");
+            const timesButton = document.getElementById("multiply");
+            const divButton = document.getElementById("divide");
+            const equalsButton = document.getElementById("equals");
+            const clearButton = document.getElementById("clear");
+            const decimalButton = document.getElementById("decimal")
+            
+            function handleOperatorClick(op) {
     if(firstNumber !== null && secondNumber !== null && operator) {
         firstNumber = operate(operator, firstNumber, secondNumber);
         secondNumber = null;
     }
-
+    
     operator = op;
     isOperatorClicked = true;
+}
+
+function updatePreviousInputDisplay(value) {
+    document.getElementById(previousOperationScreen).innerText = value
 }
 
 addButton.addEventListener("click", function() { handleOperatorClick('+');
@@ -63,53 +79,109 @@ console.log("*") });
 divButton.addEventListener("click", function() { handleOperatorClick('/'); 
 console.log("/")});
 
-clearButton.addEventListener("click", function() {
-    firstNumber = null;
-    secondNumber = null;
-    operator = null;
-    console.log("All clear!");
-});
+
+
+function handleOperationClick(op){
+    previousInput += " " + op + " ";
+    updatePreviousInputDisplay:(previousInput)
+}
 
 for(let i = 0; i <= 9; i++) {
     document.getElementById(i.toString()).addEventListener("click", function() {
-        if (isOperatorClicked) {
-            secondNumber = i;
+        
+if(isNewCalculation){
+    firstNumber = null;
+    secondNumber = null;
+    operator = null;
+    isNewCalculation = false;
+    isOperatorClicked = false;
+}
+
+
+if (isOperatorClicked) {
+            secondNumber = (secondNumber !== null) ? secondNumber.toString() + i.toString() : i.toString();
             console.log("Second number is now:", secondNumber);
+            updateDisplay(secondNumber);
+            // go back and review this code from when it was just equal to "i"
         } else {
-            firstNumber = i;
+            firstNumber = (firstNumber !== null) ? firstNumber.toString() + i.toString() : i.toString();
             console.log("First number is now:", firstNumber);
+            updateDisplay(firstNumber);
         }
+        previousInput += i.toString();
+        updatePreviousInputDisplay(previousInput);
     });
+    
+    
 }
 
 equalsButton.addEventListener("click", function() {
+    previousInput = "";
+    updatePreviousInputDisplay(previousInput);
+    
     if (firstNumber !== null && secondNumber !== null && operator !== null) {
         const result = operate(operator, firstNumber, secondNumber);
         console.log("Result", result);
+        updateDisplay(result);
         firstNumber = result;
         secondNumber = null;
         operator = null;
+        isNewCalculation = true;
         console.log("equals");
     }
 });
 
+clearButton.addEventListener("click", function() {
+previousInput = "";
+updatePreviousInputDisplay(previousInput);
+
+    firstNumber = null;
+    secondNumber = null;
+    operator = null;
+    isNewCalculation = true;
+    updateDisplay("0");
+    console.log("All clear!");
+});
+
 decimalButton.addEventListener("click", function(){
     if (isOperatorClicked){
-        if (secondNumber !== null && typeof secondNumber === "number")
-        secondNumber = secondNumber.toString();
-    }
-    if(secondNumber === null){
-        secondNumber = "0.";
-    } else if (!secondNumber.includes('.')){
-        secondNumber += '.'
-    } else {
-        if (firstNumber !== null && typeof firstNumber === "number"){
-            firstNumber = firstNumber.toString();
+        if (secondNumber === null){
+            secondNumber = "0."; 
+        } else if (!secondNumber.includes('.')){
+            secondNumber += '.';
         }
-        if (firstNumber === null){
-            firstNumber = 0;
+        updateDisplay(secondNumber);
+    } else {
+        if(firstNumber === null){
+            firstNumber = "0.";
         } else if (!firstNumber.includes('.')){
             firstNumber += '.'
-        }
+        } 
+        updateDisplay(firstNumber);
     }
+ 
+});
+
+const deleteButton = document.getElementById("deleteBtn");
+deleteButton.addEventListener("click", function(){
+    if (isOperatorClicked && secondNumber) {
+        secondNumber = secondNumber.slice(0, -1);
+        if(secondNumber === "") secondNumber = null;
+        updateDisplay(secondNumber || 0);
+    } else if (firstNumber){
+        firstNumber = firstNumber.slice(0, -1);
+        if(firstNumber === "") firstNumber = null;
+        updateDisplay(firstNumber || "0")
+    }
+});
+
+const display = document.getElementById("currentOperationScreen");
+
+function updateDisplay(value){
+    display.textContent = value;
+    console.log("Display:", value)
+}
+
 })
+
+
---
+++ t
@@ -0,0 +1,29 @@
+[33mcommit e38797018e4db9075e7a97ab841328c2ed2a6e97[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: Steam Deck User <deck@steamdeck.attlocal.net>
+Date:   Fri Sep 8 15:27:28 2023 -0400
+
+    Making progress
+
+[33mcommit 212d69646b53b32825711efd258b3a2cd116c571[m
+Author: Steam Deck User <deck@steamdeck.attlocal.net>
+Date:   Mon Sep 4 14:59:18 2023 -0400
+
+    Did it work?
+
+[33mcommit 4735ac973fcca18e60202f6c126c55680116431a[m
+Author: Steam Deck User <deck@steamdeck.attlocal.net>
+Date:   Mon Sep 4 14:01:29 2023 -0400
+
+    2nd time
+
+[33mcommit 18410cb63c25b6d40c717a67966359636ac41702[m
+Author: Steam Deck User <deck@steamdeck.attlocal.net>
+Date:   Mon Sep 4 13:34:08 2023 -0400
+
+    Hi
+
+[33mcommit 9354588c53f8f5f6fee3b6dec21d57dc4e1e3200[m
+Author: WillElizond00 <91857113+WillElizond00@users.noreply.github.com>
+Date:   Mon Sep 4 14:11:06 2023 -0400
+
+    Initial commit
